da1b5adfb351464d8d57e7889dde0ae50da6c14b
unknown <jayssendecastro@gmail.com>
2023-04-29 04:21:34 +0100

    fix one-one/many

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,19 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
-// Categories have many Products
-Category.hasMany(Product, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
-})
+});
+// Categories have many Products
+Category.hasMany(Product);
+
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, {
-  foreignKey: 'tag_id'
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
-  foreignKey: 'product_id'
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
